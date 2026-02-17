3d59c3004f598595298f37f61811436b2f914fe6
christine-49 <christine.1619@gmail.com>
2026-02-16 20:36:57 -0500

    weather api & styling

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
 console.log("hello main.js");
-
-const url = "https://api.weatherapi.com/v1/current.json?q=%22toronto%22&key=a051a60cf1ac40b6b08232626260302"
-
-// function to get current weather details from the API
-async function getWeatherDetails() {
-    const response = await fetch (url);
-    const jsonResponse = await response.json();
-
-    // extract necessary values from the API response
-    const cityName = jsonResponse.location.name;
-    const tempInCelcius = jsonResponse.current.temp_c;
-    const country = jsonResponse.location.country;
-    const currentCondition_icon = jsonResponse.current.condition.icon;
-    const currentCondition_text = jsonResponse.current.condition.text;
-
-    // innerHTML template to form HTML code and over-write existing div section
-    const placeholder = document.querySelector ("#weather-info-placeholder");
-    placeholder.innerHTML = `
-        <p> Right now it is ... </p>
-        <p>${cityName}, ${country}</p>
-        <p> 
-            <img src="${currentCondition_icon}" alt="${currentCondition_text}"/> 
-        </p>
-        <p> ${tempInCelcius} C </p>
-    `;
+//get dynamic URL
+function generateWeatherApiUrl(city, apiKey) {
+  return `https://api.weatherapi.com/v1/forecast.json?q=${city}&key=${apiKey}`;
 }
 
-getWeatherDetails();
+//function to get current weather details from the API
+async function getWeatherDetails(url) {
+  const response = await fetch(url);
+  const jsonResponse = await response.json();
+
+  //extract necessary values from the API response
+  const { name, country } = jsonResponse.location;
+  const { temp_c, condition } = jsonResponse.current;
+  const { icon, text } = condition;
+
+  const placeHolderValue = "#weather-info-placeholder"
+
+  //innerHTML template to form HTML code and over-write existing div section
+  displayWeatherInfo(name, country, temp_c, icon, text, placeHolderValue);
+}
+
+//displays a template innerHTML code
+function displayWeatherInfo(name, country, temp_c, icon, text, placeHolderValue) {
+  const placeholder = document.querySelector(placeHolderValue);
+  placeholder.innerHTML = `
+    <div class="weather-card">
+      <div class="location">${name}, ${country}</div>
+      <div class="temperature">${temp_c}°</div>
+      <div class="condition">
+        <img src="https:${icon}" alt="${text}" />
+        <span>${text}</span>
+      </div>
+    </div>
+  `;
+}
+
+//update this url to dynamically assign city and api key value
+const url = generateWeatherApiUrl("Toronto", "a051a60cf1ac40b6b08232626260302");
+
+//call entry point for our function
+getWeatherDetails(url);
